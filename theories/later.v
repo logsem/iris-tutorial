ed36e529b016c5f534aa96e24751fbf41f947d23
Lars Birkedal <birkedal@cs.au.dk>
2023-08-31 13:52:02 +0200

    later.v pass

--- theories/later.v
+++ theories/later.v
@@ -4,14 +4,14 @@ Section proofs.
 Context `{!heapGS Σ}.
 
 (**
-  Iris is a step-indexed logic, meaning it has a build in notions of
+  Iris is a step-indexed logic, meaning it has a built-in notion of
   time. This can be expressed with the later modality [▷ P] signifying
-  that [P] holds after one step. These steps are linked to the
+  that [P] holds after one time step. These time steps are linked to the
   execution steps of our programs in heaplang. Every time we use one
-  of the wp tactics, we let time tick forward. To see this, Let's look
-  at a simple program: [1 + 2 + 3]. This program takes to steps to
-  evaluate, so we can prove that if a proposition held after 2 steps
-  it will hold after the program has terminated.
+  of the wp tactics, we let time tick forward. To see this, let us look
+  at a simple program: [1 + 2 + 3]. This program takes two steps to
+  evaluate, so we can prove that if a proposition holds after 2 steps, 
+  then it will hold after the program has terminated.
 *)
 Lemma take_2_steps (P : iProp Σ) : ▷ ▷ P -∗ WP #1 + #2 + #3 {{_, P}}.
 Proof.
@@ -24,9 +24,9 @@ Qed.
 (**
   This works because later is monotone, meaning [▷ P ⊢ ▷ Q] if
   [P ⊢ Q]. Inside WP there is a later for every step of the program,
-  so the wp tactics can use monitonisity to remove laters from the
-  context. Further more, later satisfies [P ⊢ ▷ P] and
-  [▷ (P ∗ Q) ⊣⊢ ▷ P ∗ ▷ Q]. These rules allows the tactics to ignore
+  so the wp tactics can use monotonicity to remove laters from the
+  context. Furthermore, later satisfies [P ⊢ ▷ P] and
+  [▷ (P ∗ Q) ⊣⊢ ▷ P ∗ ▷ Q]. These rules allow the tactics to ignore
   hypotheses in the context that don't have a later on them.
 
   Instead of applying these rules directly, we can use the tactic
@@ -43,30 +43,30 @@ Qed.
 
 (**
   Alternatively, as later is a modality, you can use `iModIntro` and
-  therefor `iIntros "!>"` to introduce it, making it less verbose to
+  therefore `iIntros "!>"` to introduce it, making it less verbose to
   handle.
 *)
 
-(*
+(**
   With this later modality we can do a special kind of induction,
-  called löb induction. The formal statement is `□ (▷ P -∗ P) -∗ P`.
-  Intuitively this is a form of course of value induction, where we
-  say if given that `P` holds for executions strictly smaller than n
+  called Löb induction. The formal statement is `□ (▷ P -∗ P) -∗ P`.
+  Intuitively, this is a form of course of value induction, where we
+  say that if given that `P` holds for executions strictly smaller than n
   steps we can prove that `P` holds for n steps, then `P` holds for
   all steps.
 
-  We can use this to prove properties of recursive programs. To see
+  We can use principle to prove properties of recursive programs. To see
   this in action, we will define a simple recursive program that
-  recursively increments a counter.
+  increments a counter.
 *)
 
 Definition count : val :=
   rec: "count" "x" := "count" ("x" + #1).
 
-(*
+(**
   This program never terminates, as it will keep calling itself with
   larger and larger inputs. To show this we pick the post condition
-  [False]. We can now use löb induction, along with [wp_rec] to prove
+  [False]. We can now use Löb induction, along with [wp_rec], to prove
   this specification.
 *)
 Lemma count_spec (x : Z) : ⊢ WP count #x {{_, False}}.

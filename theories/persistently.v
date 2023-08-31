ccc437882b88da0c6a9ecfe2838977bc191b82c5
Lars Birkedal <birkedal@cs.au.dk>
2023-08-31 09:43:11 +0200

    persistently.v pass

--- theories/persistently.v
+++ theories/persistently.v
@@ -6,13 +6,13 @@ Context {Σ : gFunctors}.
 
 (**
   Thus far we've seen the pure context (the coq context) and the
-  spatial context. Iris proofmode has a 3rd context, called the
-  inituitionistic context or (for [iProp]) persistent context.
+  spatial context. The Iris proofmode has a third context, called the
+  inituitionistic context or (for [iProp]) the persistent context.
   These are proposisitions that act like propositions in an
   inituitionistic logic. Namely, they are reusable. These propositions
-  need not however be pure as they can still depend on resurces. Just
+  need not, however, be pure as their validity can still depend on resurces. Just
   like the pure modality, we also have a persistently modality [□ P].
-  It turns arbitrary Iris propositions into a stronger persistent
+  It turns an arbitrary Iris proposition into a weaker persistent
   proposition. Persistent proposistions are thus those [P] such that
   [P ⊢ □ P]. Iris identifies these proposisition using the typeclass
   [Persistent]. In fact, all pure propositions are persistent.
@@ -20,19 +20,19 @@ Context {Σ : gFunctors}.
 Lemma pers_ex (P Q : iProp Σ) `{!Persistent P} : P -∗ Q -∗ P ∗ Q.
 Proof.
   (**
-    We are allowed to put persistent hypotheses into the spacial
+    We are allowed to put persistent hypotheses into the spatial
     context. This will make the proofmode treat the hypothesis as
-    though it wasn't persistent.
+    though it was not persistent.
   *)
   iIntros "HP HQ".
   (**
-    The introduction pattern "#_" allows us to place a hypotheses into
+    The introduction pattern "#_" allows us to place a hypothesis into
     the persistent context.
   *)
   iDestruct "HP" as "#HP".
   iSplitR.
   - (**
-      Notice that event though we asked Iris to move all hypotheses
+      Notice that even though we asked Iris to move all hypotheses
       into the second subgoal, we still kept "HP".
     *)
     iApply "HP".
@@ -50,14 +50,17 @@ Proof.
 Qed.
 
 (**
-  Persistent propositions satisfy a lot of nice proporties simply by being duplicable [P ⊢ P ∗ P]
+  Persistent propositions satisfy a lot of nice properties,
+  simply by being duplicable [P ⊢ P ∗ P]
   For example, [P ∧ Q] and [P ∗ Q] coincide, when either [P] or [Q] is persistent.
   Likewise, [P → Q] and [P -∗ Q] coincide, when [P] is persistent.
 *)
 Check bi.persistent_and_sep.
 Check bi.impl_wand.
 
-(** Iris proofmode knows these facts, and allows [iSplit] to introduce [∗] when one of its arguements are persistent. *)
+(** The Iris proofmode knows these facts, and allows 
+  [iSplit] to introduce [∗] when one of its arguments are persistent. 
+*)
 
 
 End proofs.

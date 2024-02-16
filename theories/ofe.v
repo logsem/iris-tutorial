3cdaddbd70866fff433be30e078a79901019daf3
adamAndMath <adam.and.math@gmail.com>
2024-02-16 15:00:31 +0100

    Improve structure of ofe.v

--- theories/ofe.v
+++ theories/ofe.v
@@ -1,81 +1,40 @@
 From iris.algebra Require Export ofe.
 
 (**
-  In this file, we will look at streams of natural numbers and
-  introduce the concept of an OFE (Ordered Family of Equivalences).
-  These are types equipped with equivalences [_ ≡{n}≡ _] indexed by
-  the natural numbers through the type-class [Dist]. The key intuition
-  behind OFEs is that elements x and y are n-equivalent [x ≡{n}≡ y] if
-  they cannot be distinguished by a program running for no more than n
-  steps.
-
-  The name [dist] may seem strange as we are not talking about
-  distances, but it can be used to define an ultrametric by defining
-  the distance as 1/n when x and y are at most n-equivalent. Note
-  however, it is not necessary to know anything about metric spaces to
-  understand OFEs.
-
-  [iProp] is intuitively a kind of step-indexed propositions, meaning
-  we can equip it with an OFE structure, such that [P ≡{n}≡ Q] when
-  they agree on the first n steps. So [P ≡ Q] exactly when [P ⊣⊢ Q].
-
-  There are two important classes of functions between OFEs:
-  - [NonExpansive] functions that preserve [dist] defined as
-    [∀ n, Proper (dist n ==> dist n) f]. Intuitively this means that
-    f does not increase the steps it takes to distinguish elements.
-    However, f is allowed to decrease or maintain the steps it takes
-    to distinguish elements.
-  - [Contractive] functions satisfying
-    [∀ n, Proper (dist_later n ==> dist n) f]. Intuitively this means
-    that f always decreases the amount of steps it takes to
-    distinguish elements.
-  
-  Importantly, all the connectives we have seen on [iProp] are
-  non-expansive. This means that when working on paper, we don't need
-  To keep track of which functions are non-expansive because
-  everything you can write is non-expansive. However, in this coq
-  formalization, we can access the model of Iris and define new
-  operations that may not be non-expansive. As such, we sometimes
-  manually have to prove non-expansiveness.
-
-  [▷ _] is contractive.
-
-  A chain [c] is a cauchy sequence in the sense that
-  [∀ n i, n ≤ i → c i ≡{n}≡ c n].
-
-  A complete OFE or COFE is an OFE such that all cauchy sequences have
-  a limit.
-
-  If [A] is a COFE, then all contractive functions [f : A → A] have a
-  unique fixpoint [fixpoint f]. A fixpoint here means a value
-  satisfying [f x ≡ x]. With this, we can recursively define
-  predicates in [iProp] by guarding the recursive calls behind a
-  later.
-
-  An element [x] is called discrete if
-  [∀ y, x ≡{0}≡ y → x ≡ y]
-  This implies that all n-equivalences are just equivalence when
-  relating [x].
-
-
+  Introduce non-expansiveness and contractiveness via an example of integer streams.
+  Then relate back to Iris propositions. Mention that WP is constructed via guarded recursion.
 *)
 
-(*
+(**
+  In this file, we will go into the details of OFEs, non-expansiveness,
+  and, contractiveness. We will do this using streams of natural
+  numbers. Streams are not important to the understanding of Iris.
+  However, they form good intuition about the usefulness of OFEs.
+
   We will look at stream over natural numbers. These are infinite
   sequences with [head] being the first value, and [tail] being the
-  rest of the stream.
+  rest of the stream. We define streams as a coinductive type. You
+  should be able to follow this file without extensive knowledge of
+  coinduction.
 *)
+
 CoInductive stream := SCons {
   head : nat;
-  tail : stream
+  tail : stream;
 }.
 Add Printing Constructor stream.
 
+(**
+  Co-inductive values are allowed to be infinitely deep, meaning
+  we can construct an infinite stream of zeroes as follows:
+*)
+
 CoFixpoint zeros := SCons 0 zeros.
 
-Global Instance stream_inhabited : Inhabited stream := populate zeros.
+(**
+  [CoFixpoints] allows us to build coinductive values, in the same way
+  that [Fixpoint] allows us to use inductive types recursively.
 
-(*
   We can get the nth element of the stream by iterating through the
   stream until we find the index we are looking for.
 *)
@@ -98,91 +57,25 @@ Proof.
   - apply IHn.
 Qed.
 
-(*
-  Now, to equip our stream type with an OFE structure, we will need to
-  instantiate a couple of type-classes. to avoid polluting the
-  type-class database, we will define these instances localy in a
-  section. These instances will then only be exposed via the ofe
-  structure.
-*)
 Section ofe.
 
 (*
   We will say that two streams are equivalent when the elements are
-  equal. We can define this using the following coinductive
-  definition.
+  equal. We can specify this using [nth].
 *)
-CoInductive stream_equiv_instance : Equiv stream :=
-  | stream_equiv_scons s1 s2 : head s1 = head s2 → tail s1 ≡ tail s2 → s1 ≡ s2.
-Local Existing Instance stream_equiv_instance.
-
-Lemma stream_equiv_head (s1 s2 : stream) : s1 ≡ s2 → head s1 = head s2.
-Proof. by intros []. Qed.
-
-Lemma stream_equiv_tail (s1 s2 : stream) : s1 ≡ s2 → tail s1 ≡ tail s2.
-Proof. by intros []. Qed.
+Local Instance stream_equiv_instance : Equiv stream := λ s1 s2,
+  ∀ n, nth s1 n = nth s2 n.
 
 (*
-  This is the same as saying that the nth's elements are equal for all
-  n.
-*)
-Lemma stream_equiv_nth (s1 s2 : stream) :
-  s1 ≡ s2 ↔ ∀ n, nth s1 n = nth s2 n.
-Proof.
-  split.
-  - intros H n.
-    induction n in s1, s2, H |- *; simpl.
-    + by apply stream_equiv_head.
-    + by apply IHn, stream_equiv_tail.
-  - revert s1 s2.
-    cofix IH.
-    intros s1 s2 H.
-    apply stream_equiv_scons.
-    + apply (H 0).
-    + apply IH.
-      intros n.
-      apply (H (S n)).
-Qed.
+  Now for the OFE structure, we introduce the concept of approximate
+  equivalence using the typeclass [Dist].
 
-(*
-  Imagine a program that reads one element of the stream at every
-  step. To this program two stream will look the same for n steps if
-  the first n elements are the same. This defines a family of
-  equivalence relations as follows:
+  This typeclass defines an family of equivalence relations written
+  [[x ≡{n}≡ y]] where [n] is the precision. In our case this will
+  decide how many of the elements of the stream we look at.
 *)
 Local Instance stream_dist_instance : Dist stream := λ n s1 s2,
   ∀ i, i ≤ n → nth s1 i = nth s2 i.
-
-(*
-  These relations are written [s1 ≡{n}≡ s1] pronounced as
-  "s1 and s2 are n-equivalent".
-
-  Alternatively, we could have defined this inductively as follows:
-*)
-Lemma stream_dist_alt (n : nat) (s1 s2 : stream) : s1 ≡{n}≡ s2 ↔
-  head s1 = head s2 ∧
-  match n with
-  | 0 => True
-  | S n => tail s1 ≡{n}≡ tail s2
-  end.
-Proof.
-  split.
-  - intros H.
-    split.
-    { apply (H 0). lia. }
-    destruct n.
-    + done.
-    + intros m Hm.
-      apply (H (S m)).
-      lia.
-  - intros [H1 H2] m Hm.
-    destruct m.
-    + apply H1.
-    + destruct n.
-      * lia.
-      * apply H2.
-        lia.
-Qed.
 
 (*
   These relations should satisfy the following laws:
@@ -190,19 +83,17 @@ Qed.
   - [dist_equivalence : ∀ n, Equivalence (dist n)]
     As you would expect: The n-equivalences are equivalence relations.
   - [equiv_dist : ∀ x y, x ≡ y ↔ (∀ n, x ≡{n}≡ y)]
-    Intuitively this states that values are equivalent if and only if
-    they cannot be distinguished by a program.
+    This states that, as the "precision" of the approximation
+    approaches infinity, then the equivalences approach true
+    equivalence.
   - [dist_lt : ∀ n m x y, x ≡{n}≡ y → m < n → x ≡{m}≡ y]
-    Intuitively this states that if x and y cannot be distinguished by
-    programs running for n steps, then they cannot be distinguished by
-    programs running for less than n steps.
+    This means reducing the precision can only make more things
+    approximately equivalent.
 *)
-
 Lemma stream_ofe_mixin : OfeMixin stream.
 Proof.
   split.
   - intros s1 s2.
-    rewrite stream_equiv_nth.
     split.
     + intros H n i Hi.
       apply H.
@@ -232,27 +123,16 @@ Canonical Structure streamO := Ofe stream stream_ofe_mixin.
 End ofe.
 
 (*
-  To help with future proofs we will prove that the basic functions
-  respect our equivalence. Thereby enabling the rewrite tactic.
-*)
-Global Instance head_proper : Proper ((≡) ==> (=)) head.
-Proof. exact stream_equiv_head. Qed.
+  We can now ask the question of whether we can build values from a
+  sequence of better and better approximations. To this end we use the
+  concept of a chain. Here chains are sequences such that all elements
+  after the n'th element are n-equivalent. An OFE is complete if all
+  chains approach a value. This is also called a COFE for complete
+  OFE.
 
-Global Instance tail_proper : Proper ((≡) ==> (≡)) tail.
-Proof. exact stream_equiv_tail. Qed.
-
-Global Instance nth_proper : Proper ((≡) ==> (=) ==> (=)) nth.
-Proof.
-  intros s1 s2 H n _ <-.
-  by apply stream_equiv_nth.
-Qed.
-
-(*
-  Some OFEs are complete (called a COFE), meaning all chains have a
-  limit. Here a chain [c] is a sequence such that [c n ≡{n}≡ c i] when
-  [n ≤ i]. This means that for [stream] the [i]th stream agrees with
-  the first [i] values of all future streams. As such the limits [i]th
-  value must be the [i]th value of the [i]th stream in the chain.
+  In the case of streams, we can find the completion by considering
+  the n'th number in the n'th stream, as all streams after this point
+  agrees on this value after this point.
 *)
 Global Program Instance stream_cofe : Cofe streamO := {|
   compl c := fun2stream (λ i, nth (c i) i);
@@ -296,12 +176,64 @@ Qed.
 Global Instance SCons_ne x : NonExpansive (SCons x).
 Proof. apply contractive_ne, _. Qed.
 
+(*
+  Furthermore, all [NonExpansive] functions are also [Proper]. Meaning
+  we only need to prove the strongest of these three properties and
+  then use it for the rest.
+*)
+Global Instance SCons_proper x : Proper ((≡) ==> (≡)) (SCons x).
+Proof. apply ne_proper, _. Qed.
+
+(*
+  We don't have any such nice properties for [tail] and [nth]. So we
+  prove that they preserve the equivalence directly.
+*)
+Global Instance tail_proper : Proper ((≡) ==> (≡)) tail.
+Proof.
+  intros s1 s2 H n.
+  exact (H (S n)).
+Qed.
+
+Global Instance nth_proper : Proper ((≡) ==> (=) ==> (=)) nth.
+Proof.
+  by intros s1 s2 H n _ <-.
+Qed.
+
+(*
+  If we look at [tail] we notice that it isn't [NonExpansive] for the
+  same reason that [SCons] is contractive. However, it is
+  anti-contractive in that it decreases the "precision" by one step.
+  These functions are rarely used as they aren't [NonExpansive] and
+  therefore don't behave as well as we'd like. However, they can be
+  used when constructing new functions by balancing anti-contractive
+  functions with [Contractive] functions.
+*)
+Global Instance tail_anti_contractive : ∀ n, Proper (dist n ==> dist_later n) tail.
+Proof.
+  intros n s1 s2 H.
+  constructor.
+  intros m Hm i Hi.
+  apply (H (S i)).
+  lia.
+Qed.
+  
+
+(*
+  With the setup out of the way, we can now begin defining operations
+  on streams. The first such operation will be appending a list to the
+  front of a stream.
+*)
 Fixpoint sapp (l : list nat) (s : stream) : stream :=
   match l with
   | [] => s
   | x :: l => SCons x (sapp l s)
   end.
 
+(*
+  This operation is [NonExpansive] when we fix the list we wish to
+  prepend. Furthermore, we can prove this without unfolding the
+  definition of [dist], by using what we have proved so far.
+*)
 Global Instance sapp_ne (l : list nat) : NonExpansive (sapp l).
 Proof.
   intros n s1 s2 H.
@@ -309,6 +241,9 @@ Proof.
   - done.
   - by f_equiv.
 Qed.
+
+Global Instance sapp_proper (l : list nat) : Proper ((≡) ==> (≡)) (sapp l).
+Proof. apply ne_proper, _. Qed.
 
 (*
   Now let's define a stream that simply repeats a list over and over
@@ -356,10 +291,10 @@ Lemma repeat_with_sep_helper_unfold (l : list nat) (x : nat) (helper : list nat)
   repeat_with_sep_helper l x helper ≡ sapp helper (SCons x (repeat_with_sep_helper l x l)).
 Proof.
   induction helper as [|y helper IH].
-  - constructor; simpl.
+  - intros [|n]; simpl.
     + done.
     + done.
-  - constructor; simpl.
+  - intros [|n]; simpl.
     + done.
     + done.
 Qed.
@@ -386,8 +321,19 @@ Global Instance repeat_with_sep_contractive (l : list nat) (x : nat) : Contracti
 Proof. solve_contractive. Qed.
 
 (*
-  We can now get a fixpoint, because the [stream] is a [COFE] with an
-  inhabitant.
+  To get a fixpoint for such a function we need three things
+  - The type we wish to produce an element of (stream) must have a
+    COFE structure, which we defined earlier.
+  - The function we wish to find a fixpoint for must be contractive,
+    which we just proved.
+  - Finally, the COFE must be inhabited, meaning there is an element
+    of that type. We can use [zeroes] for this, as the choice of
+    inhabitant doesn't matter.
+*)
+Global Instance stream_inhabited : Inhabited stream := populate zeros.
+
+(*
+  With all conditions met, we can now define the fixpoint as follows:
 *)
 Definition repeat_with_sep_alt (l : list nat) (x : nat) :=
   fixpoint (repeat_with_sep_pre l x).
@@ -409,3 +355,148 @@ Proof.
   apply fixpoint_unique.
   apply repeat_with_sep_unfold.
 Qed.
+
+(*
+  A common operation one could consider on streams is mapping. This
+  luckily conforms to the rules of co-fixpoints, but we will also
+  define it using guarded recursion.
+*)
+CoFixpoint stream_map (f : nat → nat) (s : stream) : stream :=
+  SCons (f (head s)) (stream_map f (tail s)).
+
+(*
+  Notice that our recursive call takes different parameters. So we
+  can't just build this as a fixpoint on streams. Instead, we need to
+  build a fixpoint on [stream → stream]. However, this is not equipped
+  with a canonical OFE structure. Instead, we need to write
+  [stream -d> streamO]. Here "d" is short for discrete, meaning we
+  don't consider any OFE structure on the domain. As such, we are
+  allowed to change the parameter arbitrarily on recursive calls.
+*)
+Definition stream_map_pre (f : nat → nat) (map : stream -d> streamO) : stream -d> streamO :=
+  λ s : stream, SCons (f (head s)) (map (tail s)).
+
+Global Instance stream_map_pre_contractive (f : nat → nat) : Contractive (stream_map_pre f).
+Proof. solve_contractive. Qed.
+
+(*
+  In this way, we can use guarded-fixpoints as an alternative to
+  co-fixpoints. Allowing us to use existing functions in the
+  fixpoint definitions.
+*)
+Definition stream_map_alt (f : nat → nat) := fixpoint (stream_map_pre f).
+
+(*
+  Rather than simply restating the fixpoint unfolding lemma directly,
+  we will fully apply the function. 
+*)
+Lemma stream_map_alt_unfold (f : nat → nat) (s : stream) :
+  stream_map_alt f s ≡ SCons (f (head s)) (stream_map_alt f (tail s)).
+Proof. exact (fixpoint_unfold (stream_map_pre f) s). Qed.
+
+Lemma stream_map_alt_correct (f : nat → nat) (s : stream) : stream_map f s ≡ stream_map_alt f s.
+Proof.
+  apply (fixpoint_unique (stream_map_pre f)).
+  clear s; intros s.
+  intros [|n]; simpl.
+  + done.
+  + done.
+Qed.
+
+Lemma stream_map_nth (f : nat → nat) (s : stream) (n : nat) :
+  nth (stream_map f s) n = f (nth s n).
+Proof.
+  induction n in s |- *; simpl.
+  - done.
+  - apply IHn.
+Qed.
+
+Global Instance stream_map_ne (f : nat → nat) : NonExpansive (stream_map f).
+Proof.
+  intros n s1 s2 H m Hm.
+  rewrite !stream_map_nth.
+  f_equal.
+  apply H, Hm.
+Qed.
+
+(*
+  If we now wanted to create a stream of all the powers of 2, we would
+  intuitively want something like this:
+*)
+Fail CoFixpoint power2 : stream :=
+  SCons 1 (stream_map (λ n, n * 2) power2).
+
+(*
+  However, this again fails the syntactic check, as we are modifying
+  the tail of the stream.
+
+  To fix this we could instead define a stream starting with a number
+  [n] doubling [n] at every step.
+*)
+CoFixpoint power2_helper (n : nat) : stream :=
+  SCons n (power2_helper (n * 2)).
+Definition power2 : stream :=
+  power2_helper 1.
+
+(*
+  To know that these definitions are equivalent, we must again prove
+  that [power2_helper] and [power2] satisfy respective fixpoint
+  equations.
+*)
+Lemma power2_helper_unfold (n : nat) :
+  power2_helper n ≡ SCons n (stream_map (λ n, n * 2) (power2_helper n)).
+Proof.
+  intros [|i]; simpl.
+  { done. }
+  rewrite stream_map_nth.
+  induction i in n |- *; simpl.
+  - done.
+  - done.
+Qed.
+
+Lemma power2_unfold :
+  power2 ≡ SCons 1 (stream_map (λ n, n * 2) power2).
+Proof. apply power2_helper_unfold. Qed.
+
+(*
+  Now, just like before, we can instead define the function for which
+  we wish to find a fixpoint:
+*)
+Definition power2_pre (s : stream) : stream :=
+  SCons 1 (stream_map (λ n, n * 2) s).
+
+(*
+  This is again contractive in [s], because [SCons] is [Contractive]
+  and [stream_map] is [NonExpansive].
+*)
+Global Instance power2_pre_contractive : Contractive power2_pre.
+Proof. solve_contractive. Qed.
+
+(* So we can now get a fixpoint, just as before *)
+Definition power2_alt := fixpoint power2_pre.
+
+Lemma power2_alt_unfold : power2_alt ≡ power2_pre power2_alt.
+Proof. apply fixpoint_unfold. Qed.
+
+(*
+  Like before, we know that this fixpoint is unique, so it must be
+  equivalent to the other definition.
+*)
+Lemma power2_alt_correct : power2 ≡ power2_alt.
+Proof.
+  apply fixpoint_unique.
+  apply power2_unfold.
+Qed.
+
+(**
+  [iProp] also has a COFE structure, but it is not a co-inductive
+  type. This means that even though we can't use [CoFixpoint] to build
+  propositions recursively, we can use [fixpoint]. Importantly, all
+  the connectives of the logic are [NonExpansive]. To then use guarded
+  fixpoints, we need a [Contractive] function like [SCons]. And for
+  [iProp] this is later [▷ P]. We have already run into two
+  connectives defined using the guarded-fixpoint, namely the weakest-
+  precondition [WP], and invariants [inv]. This is why we can remove
+  laters from premises when taking a step in the program, as this
+  corresponds to unfolding the fixpoint, thus revealing a later.
+*)

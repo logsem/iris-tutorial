9aa7d3f37780cc4f5a2a891313c5377efdcd8ac5
Mathias Pedersen <mathiasp9999@gmail.com>
2024-08-27 14:41:59 +0200

    Added chapter title. Reformulated some comments. Formatting.

--- exercises/gr_predicates.v
+++ exercises/gr_predicates.v
@@ -1,24 +1,27 @@
 From iris.bi Require Export fixpoint.
 From iris.heap_lang Require Import lang proofmode notation.
 
+(* ################################################################# *)
+(** * Guarded Recursive Predicates *)
+
 (**
-  In this file, we present an alternative approach to defining representation
-  predicates for linked lists, which uses fixpoints of predicates rather than
-  Coq's Fixpoint mechanism.
-  The high-level point to notice is that we can define fixpoints of monotone
-  functions on Iris predicates, much in the same way as one can define fixpoints
-  of monotone function on Coq predicates [XXX insert reference, or include an
-  example.]
-  Later on, we will discuss different kinds of fixpoints in more detail.
- *)
+  In the Linked List chapter, we defined a representation predicate for
+  linked lists using Coq's Fixpoint mechanism. In this chapter, we
+  present an alternative approach to defining representation predicates,
+  which instead uses fixpoints of predicates. The high-level point to
+  notice is that we can define fixpoints of monotone functions on Iris
+  predicates, much in the same way as one can define fixpoints of
+  monotone function on Coq predicates. Later on, we will discuss
+  different kinds of fixpoints in more detail.
+*)
 
 Section proofs.
 Context `{!heapGS Σ}.
 
 (**
   As we have already seen, we can define a predicate for linked lists
-  representing a list of specific values, using Coq's notion of
-  fixpoint for the inductive Coq type [list val].
+  representing a list of specific values, using Coq's notion of fixpoint
+  for the inductive Coq type [list val].
 *)
 Fixpoint is_list_of (v : val) (xs : list val) : iProp Σ :=
   match xs with
@@ -27,27 +30,28 @@ Fixpoint is_list_of (v : val) (xs : list val) : iProp Σ :=
   end.
 
 (**
-  However, sometimes we don't care about the exact list and instead,
-  we only want to know that each value of the list satisfies a predicate.
-  This can be captured by using a helper predicate [all] expressing that
-  all elements of a Coq list satisfy a predicate.
-  Using that, we can then simply state that the value is represented by one such
-  list.
+  However, sometimes we don't care about the exact list and instead, we
+  only want to know that each value of the list satisfies some
+  predicate. This can be captured by using a helper predicate [all]
+  expressing that all elements of a Coq list satisfy a predicate.
 *)
-
 Fixpoint all (xs : list val) (Φ : val → iProp Σ) : iProp Σ :=
   match xs with
   | [] => True
   | x :: xs => Φ x ∗ all xs Φ
   end.
 
+(**
+  Then, using [all], we can express that a value represents _some_
+  list whose elements satisfy a given predicate.
+*)
 Definition is_list (v : val) (Φ : val → iProp Σ) : iProp Σ :=
   ∃ xs, is_list_of v xs ∗ all xs Φ.
 
 (**
   However, this definition is rather annoying to work with, as it
-  requires explicitly finding the list of values. Alternatively, we
-  can define the is_list predicate as the solution to a recursive
+  requires explicitly finding the list of values. Alternatively, we can
+  define the [is_list] predicate as the solution to a recursive
   definition. This means defining a function:
 
     [F : (A → iProp Σ) → (A → iProp Σ)]
@@ -59,8 +63,8 @@ Definition is_list_pre (Φ : val → iProp Σ) (f : val → iProp Σ) (v : val) 
   ⌜v = NONEV⌝ ∨ ∃ l : loc, ⌜v = SOMEV #l⌝ ∗ ∃ x t, l ↦ (x, t) ∗ Φ x ∗ f t.
 
 (**
-  Recursive definitions can have multiple fixpoints. Of these, there
-  are two special fixpoints: The least fixpoint and the greatest fixpoint.
+  Recursive definitions can have multiple fixpoints. Of these, there are
+  two special fixpoints: the least fixpoint and the greatest fixpoint.
   The least fixpoint corresponds to an inductively defined predicate,
   while the greatest corresponds to a coinductively defined predicate.
 
@@ -81,31 +85,37 @@ Proof.
       iApply ("HΨ" with "Ht").
   - (**
       In addition to monotonicity, we also need to prove that the
-      resulting predicate is 'time preserving' (NonExpansive).
-      (We will explain NonExpansiveness etc. in more detail later.)
-      This is trivial in this case as values are discrete.
+      resulting predicate is `time preserving' – [NonExpansive]. We will
+      explain non-expansiveness in more detail later. In this case, it
+      is trivial as values are discrete.
     *)
     apply _.
 Qed.
 
 (**
-  Now that we have proved monotonicity, we can obtain the least fixed point,
-  and lemmas for its unfolding and an induction principle.
+  Now that we have proved monotonicity, we can obtain the least fixed
+  point along with associated lemmas for its unfolding and an induction
+  principle.
 *)
 
-Definition is_list_rec (v : val) (Φ : val → iProp Σ) := bi_least_fixpoint (is_list_pre Φ) v.
+Definition is_list_rec (v : val) (Φ : val → iProp Σ) :=
+  bi_least_fixpoint (is_list_pre Φ) v.
 
-Lemma is_list_rec_unfold (v : val) (Φ : val → iProp Σ) : is_list_rec v Φ ⊣⊢ is_list_pre Φ (λ v, is_list_rec v Φ) v.
+Lemma is_list_rec_unfold (v : val) (Φ : val → iProp Σ) :
+  is_list_rec v Φ ⊣⊢ is_list_pre Φ (λ v, is_list_rec v Φ) v.
 Proof. apply least_fixpoint_unfold, _. Qed.
 
-Lemma is_list_rec_ind (Φ Ψ : val → iProp Σ) : □ (∀ v, is_list_pre Φ Ψ v -∗ Ψ v ) -∗ ∀ v, is_list_rec v Φ -∗ Ψ v.
+Lemma is_list_rec_ind (Φ Ψ : val → iProp Σ) :
+  □ (∀ v, is_list_pre Φ Ψ v -∗ Ψ v ) -∗ ∀ v, is_list_rec v Φ -∗ Ψ v.
 Proof. apply least_fixpoint_iter, _. Qed.
 
 (**
-  We can now prove that our new definition is_list_rec obtained as the fixpoint
-  is equivalent to the definition we gave earlier.
+  Of course, this new representation predicate, [is_list_rec], should be
+  equivalent to our original definition, [is_list]. We can indeed prove
+  that this is the case.
 *)
-Lemma is_list_rec_correct (v : val) (Φ : val → iProp Σ) : is_list v Φ ⊣⊢ is_list_rec v Φ.
+Lemma is_list_rec_correct (v : val) (Φ : val → iProp Σ) :
+  is_list v Φ ⊣⊢ is_list_rec v Φ.
 Proof.
   iSplit.
   - iIntros "(%xs & Hv & HΦs)".

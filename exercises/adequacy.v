b58b266c9d859d0347c50ac514f1282a9d99ed0a
MatteP1 <mathiasp9999@gmail.com>
2024-08-28 14:43:53 +0200

    Added chapter title. Clarifications and rephrasings.

--- exercises/adequacy.v
+++ exercises/adequacy.v
@@ -3,15 +3,18 @@ From iris.base_logic.lib Require Export token.
 From iris.heap_lang Require Import adequacy lang proofmode notation par.
 From exercises Require Import spin_lock.
 
+(* ################################################################# *)
+(** * Adequacy *)
+
 (**
-  Thus far we've assumed that the weakest precondition is correct. But
-  what does that even mean? This is what is called adequacy, and its
-  definition depends on the properties we want it to satisfy. In Iris
-  we've decided to focus on two properties:
+  Thus far, we have assumed that the weakest precondition is correct.
+  But what does that even mean? This is what is called _adequacy_, and
+  its definition depends on the properties we want it to satisfy. In
+  Iris, we have decided to focus on two properties:
   - The program won't get stuck.
   - The program satisfies the postcondition.
 
-  In this file, we'll go over how to define these properties and show
+  In this chapter, we go over how to define these properties and show
   that the weakest precondition satisfies them.
 
   HeapLang is defined using a step relation. This relation defines how
@@ -19,24 +22,24 @@ From exercises Require Import spin_lock.
 
     [prim_step e1 σ1 κs e2 σ2 efs]
 
-  Here [e1] is the expression we want to reduce, and [σ1] is the
+  Here, [e1] is the expression we want to reduce, and [σ1] is the
   current state of the machine. [e2] and [σ2] are then the new
-  expression and state. [efs] is a list of expressions representing new
-  threads spawned. Finally [κs] represents a list of something called
-  observations. We won't use these, so κs will remain empty.
+  expression and new state. [efs] is a list of expressions representing
+  new threads spawned. Finally [κs] represents a list of something
+  called observations. We won't use these, so [κs] will remain empty.
 
-  This relation isn't very nice to work with by itself. To fix this
-  we will package the state of the program into what we will call a
-  configuration. Notice that this configuration takes a list of
-  expressions. These represent all the threads that are currently running.
+  This relation isn't very nice to work with by itself. To fix this, we
+  package the state of the program into what we call a configuration.
+  Notice that this configuration takes a list of expressions. These
+  represent all the threads that are currently running.
 *)
 
 Definition cfg : Type := list expr * state.
 
 (**
-  Now let's define steps in terms of configurations. To model the
-  non-sequential Steps behaviour of threads, we define steps to be
-  a step in any thread.
+  Now, let us define steps in terms of configurations. To model the
+  non-sequential behaviour of threads, we define steps to be a step in
+  any thread.
 *)
 Inductive step (ρ1 ρ2 : cfg) : Prop :=
   step_atomic e1 σ1 e2 σ2 efs t1 t2 :
@@ -49,7 +52,7 @@ Inductive step (ρ1 ρ2 : cfg) : Prop :=
   With this, we can define multiple steps as the reflexive transitive
   closure ([rtc]) of [step].
 
-  Now a program is stuck if any thread can't take a step and isn't a
+  Now, a program is stuck if any thread cannot take a step and is not a
   value. Conversely, a thread is not stuck if it is either a value or
   can take a step.
 *)
@@ -57,44 +60,54 @@ Definition not_stuck e σ :=
   is_Some (to_val e) ∨ ∃ e' σ' efs, prim_step e σ [] e' σ' efs.
 
 (**
-  So a program won't get stuck if all possible future configurations
-  aren't stuck.
+  With this, we can now express the first adequacy criterion: a program
+  will not get stuck if all possible future configurations are not
+  stuck.
 
   [∀ t σ' e', rtc step ([e], σ) (t, σ') → e' ∈ t → not_stuck e' σ']
 
-  Likewise, a program satisfies a postcondition when the main thread
-  terminates to a value, that value satisfies that postcondition.
+  Likewise, for the second criterion, we say that a program satisfies a
+  postcondition when the main thread terminates to a value, and that
+  value satisfies the postcondition.
 
   [∀ t σ' v, rtc step ([e], σ) (of_val v :: t, σ') → φ v σ']
 
-  These two properties together define [adequate]. However [adequate]
-  takes an extra parameter of type [stuckness]. This parameter
-  specifies whether adequacy includes the first condition.
+  Together, these two properties define [adequate]. However, [adequate]
+  takes an extra parameter of type [stuckness]. This parameter specifies
+  whether adequacy includes the first criterion.
 
   The proof that the weakest precondition implies adequacy is:
 
   [[
-    heap_adequacy Σ `{!heapGpreS Σ} s e σ φ :
-      ∀ _ : heapGS Σ, inv_heap_inv -∗ WP e @ s; ⊤ {{ v, ⌜φ v⌝}} →
-      adequate s e σ (λ v σ, φ v)
+    heap_adequacy Σ `{!heapGpreS Σ} s e σ φ : ∀ _ : heapGS Σ,
+      inv_heap_inv -∗ WP e @ s; ⊤ {{ v, ⌜φ v⌝}} → adequate s e σ (λ v σ,
+      φ v)
   ]]
 
-  To use this statement we have to actually instantiate Σ. It needs to
-  contain all the kinds of resources we needed to prove the weakest
-  precondition. Let's use [prog] from spin_lock. To prove [prog_spec]
-  we used the resources of heaplang as well as [exclR unitO]. The
-  resources of heaplang are all contained in [heapΣ]. For the camera,
-  we use GFunctor to turn it into a gfunctor.
+  To use this statement, we have to actually instantiate [Σ]. It needs
+  to contain all the kinds of resources we needed to prove the weakest
+  precondition. Let us use [prog] from the Spin Lock chapter as an
+  example. To prove [prog_spec], we used the resource of heaps as well
+  as tokens. The resources needed by the resource of heaps are all
+  contained in [heapΣ]. Likewise, the resources needed by token
+  ([exclR unitO]) are contained in [tokenΣ].
 *)
-Definition progΣ := #[
-  heapΣ; (* The resources required for heaplang itself *)
-  tokenΣ (* The camera we used for spin-lock *)
+
+Definition progΣ : gFunctors := #[
+  heapΣ; (* The resources required by HeapLang *)
+  tokenΣ (* The resources we used for spin-lock *)
 ].
 
-(** Finally we can combine the pieces to prove adequacy. *)
-Lemma prog_adequacy σ : adequate NotStuck prog σ (λ v _, v = #0 ∨ v = #1).
+(** Finally, we can combine the pieces to prove adequacy. *)
+Lemma prog_adequacy σ :
+  adequate NotStuck prog σ (λ v _, v = #0 ∨ v = #1).
 Proof.
   apply (heap_adequacy progΣ).
   iIntros "%_ _".
   iApply prog_spec.
 Qed.
+
+(** 
+  With this, we now have a _closed_ proof that [prog] does not get
+  stuck, and if it terminates at some value [v], then [v = #0 ∨ v = #1].
+*)
